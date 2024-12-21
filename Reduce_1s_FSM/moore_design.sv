4c97eca8859176abf91c15f82d3642aff983638e
prxnav2005 <pranavmofficial2005@gmail.com>
2024-12-21 19:19:42 +0530

    Reduce_1s_FSM moore done

---
+++ Reduce_1s_FSM/moore_design.sv
@@ -0,0 +1,42 @@
+// Code your design here
+module fsm(input logic clk, in, output logic out);
+  parameter logic [1:0] zero = 2'b00, one1 = 2'b01, two1s = 2'b10; 
+  logic [1:0] state, next_state;
+  
+  always_ff@(posegde clk)
+    begin
+      state <= next_state;
+    end
+  
+  always_comb
+    begin
+      next_state = state;
+      out = 0;
+      case(state)
+        zero : begin
+          out = 0;
+          if(in)
+            next_state = one1;
+          else
+            next_state = zero;
+        end
+        
+        one1: begin
+          out = 0;
+          if(in)
+            next_state = two1s;
+          else
+            next_state = zero;
+        end
+        
+        two1s: begin
+          out = 1;
+          if(in)
+            next_state = two1s;
+          else
+            next_state = zero;
+        end
+      endcase
+    end
+endmodule
+        
