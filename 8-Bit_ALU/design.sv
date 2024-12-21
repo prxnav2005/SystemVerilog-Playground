d0ddaf009b67244e488bf1c09eab548ff420c5b3
prxnav2005 <pranavmofficial2005@gmail.com>
2024-12-21 15:41:57 +0530

    ALU done

---
+++ 8-Bit_ALU/design.sv
@@ -0,0 +1,13 @@
+module ALU #(parameter N = 8)(input logic [N-1:0] a,b, input logic [1:0] op_code, output logic [N-1:0] res);
+  
+  always_comb
+    begin
+      case(op_code)
+        2'b00 : res = a&b;
+        2'b01 : res = a^b;
+        2'b10 : res = a+b;
+        2'b11 : res = a-b;
+        default : res = 0;
+      endcase
+    end
+endmodule
---
+++ 8-Bit_ALU/tb.sv
@@ -0,0 +1,27 @@
+module tb_ALU;
+  logic [7:0] A, B, result;
+  logic [1:0] op_code;
+  ALU #(8) alu_inst (.A(A), .B(B), .op_code(op_code), .result(result));
+
+  initial begin
+    A = 8'b00000001;
+    B = 8'b00000010;
+    op_code = 2'b00;
+    #10;
+    assert(result == (A & B)) else $fatal("Test failed for AND operation");
+
+    op_code = 2'b01;
+    #10;
+    assert(result == (A ^ B)) else $fatal("Test failed for XOR operation");
+
+    op_code = 2'b10;
+    #10;
+    assert(result == (A + B)) else $fatal("Test failed for ADD operation");
+
+    op_code = 2'b11;
+    #10;
+    assert(result == (A - B)) else $fatal("Test failed for SUB operation");
+
+    $finish;
+  end
+endmodule
